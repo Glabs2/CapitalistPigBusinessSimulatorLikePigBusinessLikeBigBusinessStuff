83910077d823d278e7a8fd8c80ac448986f3dad2
Tom <llamato@toms-macbook.speedport_w723_v_typ_a_1_01_018>
2018-07-04 16:57:43 +0200

    Updateing repo for sync. Files not finished!i

---
+++ capitalist_math.py
@@ -0,0 +1,22 @@
+import random as r
+
+class CapMath:
+
+	def __init__(self):
+		#Debug!!!
+		print("import successful")
+
+
+    def calculate_customers(self,pr_val,employees):
+    	customers = (pr_val + employees * r.uniform(0.8,1.3)) * r.uniform(0.9,1.2) 
+    	return customers
+
+
+
+
+
+
+
+
+
+
